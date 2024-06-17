9b19b05a242aec725f0c871e42e834e4e5d5f0ed
MorFyNC <bulat.hisambeev@gmail.com>
2024-06-17 16:02:30 +0300

    1й

---
+++ MediaPlayer/DataBase/DataBaseManager.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Win32;
+
+namespace MediaPlayer.DataBase
+{
+    public static class DataBaseManager
+    {
+        private static MediaPlayerEntities _dataBase = new MediaPlayerEntities();
+
+        public static List<Song> GetAllSongs()
+        {
+            return _dataBase.Song.ToList();
+        }
+
+        public static List<Playlist> GetAllPlayLists()
+        {
+            return _dataBase.Playlist.ToList();
+        }
+
+        public static void AddSong(Song song)
+        {
+            _dataBase.Song.Add(song);
+            SaveChanges();
+        }
+
+        public static void AddPlayList(Playlist playlist)
+        {
+            _dataBase.Playlist.Add(playlist);
+            SaveChanges();
+        }
+
+        public static void RemovePlayList(Playlist playlist)
+        {
+            _dataBase.Playlist.Remove(playlist);
+            SaveChanges();
+        }
+
+        public static void UpdatePlayList(Playlist playlist)
+        {
+            _dataBase.Playlist.AddOrUpdate(playlist);
+            SaveChanges();
+        }
+        public static void RemoveSong(Song song)
+        {
+            _dataBase.Song.Remove(song);
+            SaveChanges();
+        }
+
+        public static void SaveChanges()
+        {
+            try
+            {
+                _dataBase.SaveChanges();
+            }
+            catch (DbEntityValidationException ex)
+            {
+                var sb = new StringBuilder();
+
+                foreach (var f in ex.EntityValidationErrors)
+                {
+                    sb.AppendFormat("{0} fail validation\n", f.Entry.Entity.GetType());
+                    foreach (var err in f.ValidationErrors)
+                    {
+                        sb.AppendFormat("- {0} : {1}", err.PropertyName, err.ErrorMessage);
+                        sb.AppendLine();
+                    }
+                }
+
+                throw new DbEntityValidationException("Fail: " + sb.ToString(), ex);
+            }
+        }
+    }
+}
--- MediaPlayer/Windows/MainWindow.xaml.cs
+++ MediaPlayer/Windows/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using Microsoft.Win32;
 using System.Windows.Threading;
+using MediaPlayer.DataBase;
 
 namespace MediaPlayer
 {
@@ -9,7 +11,7 @@ namespace MediaPlayer
     {
         private DispatcherTimer timer;
         private Queue queueWindow;
-
+        private Queue<Song> SongQueue;
         public MainWindow()
         {
             InitializeComponent();
@@ -23,7 +25,7 @@ namespace MediaPlayer
         private void AddFile_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "Media files (*.mp4;*.mp3)|*.mp4;*.mp3|All files (*.*)|*.*";
+            openFileDialog.Filter = "Media files (*.mp3)|*.mp3|All files (*.*)|*.*";
             if (openFileDialog.ShowDialog() == true)
             {
                 string fileName = openFileDialog.FileName;
@@ -36,6 +38,7 @@ namespace MediaPlayer
 
         private void OpenQueue_Click(object sender, RoutedEventArgs e)
         {
+            queueWindow = new Queue();
             queueWindow.Show();
         }
 
@@ -96,6 +99,12 @@ namespace MediaPlayer
             {
                 mediaElement.Position = TimeSpan.FromSeconds(timelineSlider.Value);
             }
+        }
+
+        private void Playlist_Click(object sender, RoutedEventArgs e)
+        {
+            //PlayerWindow p = new PlayerWindow();
+            //p.Show();
         }
     }
 }
